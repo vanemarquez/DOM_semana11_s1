e0c5020530cfb6e224460b84d06b069a882a0ec1
Vanessa Marquez <vaneyz0302@gmail.com>
2024-05-27 22:06:54 -0500

    Se agregó y modificó contenido en app.js index.html readme.md styles.css

---
+++ app.js
@@ -0,0 +1,26 @@
+//DOM
+//Permite a los desarrolladores acceder y modificar los elementos de la página utilizando código JavaScript.
+
+//getElements
+//getElementById: Es una función, que permite seleccionar un único elemento de una página web basándonos en su atributo de identificación (ID).
+//querySelector: Se utiliza para seleccionar un elemento del DOM, utilizando un selector CSS. Devuelve el primer elemento que coincide con el selector especificado.
+//querySelectorAll: Es similar a querySelector, pero en lugar de devolver solo el primer elemento que coincide con el selector especificado, devuelve una NodeList (una lista de nodos) que contiene todos los elementos que coinciden con el selector.
+
+//modifiedElements
+//textContent: Te permite acceder al contenido textual de un elemento HTML y modificarlo si es necesario.
+//innerHTML: Te permite acceder y modificar el contenido HTML dentro de un elemento HTML.
+//En resumen, textContent maneja solo texto dentro de un elemento, mientras que innerHTML maneja y permite modificar el contenido HTML dentro de un elemento, incluyendo etiquetas HTML.
+
+//styles
+//property classList.add: Te permite añadir una clase CSS a un elemento HTML. Si el elemento ya tiene la clase especificada, classList.add no hará nada.
+//property classList.remove: Te permite eliminar una clase CSS de un elemento HTML. Si el elemento no tiene la clase especificada, classList.remove no hará nada.
+
+//Dom Traversing
+//Es el proceso de moverse a través de los elementos del DOM. Para poder encontrar, acceder o manipular elementos específicos.
+//parentElement:  Te da acceso al elemento padre directo del elemento actual.
+//nextElementsSibling: Te da acceso al siguiente hermano del elemento actual en el mismo nivel de jerarquía.
+//children: Te da acceso a todos los elementos hijos directos del elemento actual.
+
+//Create element
+//createElement: Se utiliza para crear un nuevo elemento HTML en el DOM. 
+//Este método te permite crear elementos como divs, párrafos, encabezados, etc., y luego agregarlos a tu página web.
---
+++ readme.md
@@ -0,0 +1,26 @@
+
+# **DOM**
+Permite a los desarrolladores acceder y modificar los elementos de la página utilizando código JavaScript.
+
+## **getElements**
+- **getElementById:** Permite seleccionar un único elemento de una página web basándonos en su (ID).
+- **querySelector:** Devuelve el primer elemento que coincide con el selector especificado.
+- **querySelectorAll:** Es similar a `querySelector`, pero en lugar de devolver solo el primer elemento que coincide con el selector especificado, devuelve todos los elementos que coinciden con el selector.
+
+## **modifiedElements**
+- **textContent:** Te permite acceder al contenido textual de un elemento HTML y modificarlo si es necesario.
+- **innerHTML:** Te permite acceder y modificar el contenido HTML dentro de un elemento HTML.
+En resumen, `textContent `maneja solo texto dentro de un elemento, mientras que `innerHTML`  maneja y permite modificar el contenido HTML dentro de un elemento, incluyendo etiquetas HTML.
+
+## **styles**
+- **property classList.add:** Te permite añadir una clase CSS a un elemento HTML. 
+- **property classList.remove:** Te permite eliminar una clase CSS de un elemento HTML. 
+
+## **Dom Traversing**
+- **parentElement:**  Te da acceso al elemento padre directo del elemento actual.
+- **nextElementsSibling:** Te da acceso al siguiente hermano del elemento actual en el mismo nivel de jerarquía.
+- **children:** Te da acceso a todos los elementos hijos directos del elemento actual.
+
+## **Create element**
+- **createElement:** Se utiliza para crear un nuevo elemento HTML. Este método te permite crear elementos como divs, párrafos, encabezados, etc., y luego agregarlos a tu página web.
+
